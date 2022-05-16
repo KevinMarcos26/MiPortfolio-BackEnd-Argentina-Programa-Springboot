e7ff76ce0e81b37761bb9c9319fda7771b04b934
KevinMarcos26 <100862089+KevinMarcos26@users.noreply.github.com>
2022-05-16 20:40:27 -0300

    Update Educacion.java

--- SpringbootPortfolio/src/main/java/com/MiPortfolio/SpringbootPortfolio/Controller/Educacion.java
+++ SpringbootPortfolio/src/main/java/com/MiPortfolio/SpringbootPortfolio/Controller/Educacion.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://arcane-stack-350307.web.app/")
 @RestController
 @RequestMapping("/educacion")
 public class Educacion {
